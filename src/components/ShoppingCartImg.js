914020093d93238a4c0f78b72bacc97fd752380a
Andre Graczyk <andregraczyk@outlook.com>
2021-08-09 17:15:51 -0300

    lint e ShoppingCart em pages

--- src/components/ShoppingCartImg.js
+++ src/components/ShoppingCartImg.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
- class ShoppingCartImg extends React.Component {
-   render () {
+class ShoppingCartImg extends React.Component {
+  render() {
     return (
       <img src="https://image.flaticon.com/icons/png/512/126/126510.png" alt="shopping cart icon" />
-    )
+    );
   }
 }
 
