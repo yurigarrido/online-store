81f31acadb7c549222fbdec8a10db0ccec64e4b6
Emídio Rebouças <emidioreb@gmail.com>
2021-08-09 18:56:44 -0300

    requeriment 4

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
+import * as api from '../services/api';
+import Categories from './Categories';
 
 class Main extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories()
+      .then((resolve) => this.setState({ categories: resolve }));
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <nav>
+          <h5>Categorias:</h5>
+          {categories
+            .map((category) => <Categories key={ category.id } category={ category } />)}
+        </nav>
       </div>
     );
   }
