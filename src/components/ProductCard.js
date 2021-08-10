e1c0a2e8e6391883d2865a25cda18e4adc755c4d
V. Dionysio <vini.dionysio@gmail.com>
2021-08-10 17:14:50 -0300

    Solving requirement 07

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ProductCard extends React.Component {
   render() {
     const { item } = this.props;
-    const { thumbnail, title, price } = item;
+    const { thumbnail, title, price, id } = item;
     return (
-      <section data-testid="product">
-        <h3>
-          { title }
-        </h3>
-        <img src={ thumbnail } alt={ title } />
-        <p>
-          { price }
-        </p>
-      </section>
+      <Link data-testid="product-detail-link" to={ `/product-details/${id}` }>
+        <section data-testid="product">
+          <h3>
+            { title }
+          </h3>
+          <img src={ thumbnail } alt={ title } />
+          <p>
+            { price }
+          </p>
+        </section>
+      </Link>
     );
   }
 }
@@ -26,5 +29,6 @@ ProductCard.propTypes = {
     title: PropTypes.string,
     thumbnail: PropTypes.string,
     price: PropTypes.number,
+    id: PropTypes.string,
   }).isRequired,
 };
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ProductDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      item: {},
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+
+    this.getItemInformation(id)
+      .then((item) => this.setState({ item }));
+  }
+
+  async getItemInformation(id) {
+    const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+    const data = await response.json();
+
+    return data;
+  }
+
   render() {
+    const { item } = this.state;
     return (
       <div>
-        ProductDetails
+        <h3 data-testid="product-detail-name">
+          { item.title }
+          <span>
+            { ` - ${item.price}` }
+          </span>
+        </h3>
+        <img src={ item.thumbnail } alt={ item.title } />
+        <div>
+          <h4>Especificação técnica</h4>
+          <ul>
+            <li>Em construção</li>
+          </ul>
+        </div>
       </div>
     );
   }
 }
+
+ProductDetails.propTypes = {
+  match: PropTypes.shape(
+    { params: PropTypes.shape(
+      { id: PropTypes.string },
+    ) },
+  ).isRequired,
+};
 
 export default ProductDetails;
